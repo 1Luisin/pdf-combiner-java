7fcab91c6d4c0d1424092bcb888e9dc783bfce3d
Luis Guilherme <guilherme32698@gmail.com>
2025-08-01 09:17:48 -0300

    melhorando menu, opções, coerencia e legibilidade do codigo

---
+++ src/ClearConsole.java
@@ -0,0 +1,15 @@
+public class ClearConsole {
+
+    public static void Limpador() {
+        try {
+            String os = System.getProperty("os.name");
+            if (os.contains("Windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                Runtime.getRuntime().exec("clear");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/PdfCombiner.java
+++ src/PdfCombiner.java
@@ -1,30 +1,39 @@
 import org.apache.pdfbox.multipdf.PDFMergerUtility;
+
+import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class PdfCombiner{
-    public static void main(String[] args) {
 
+    public static final String ANSI_RED = "\u001B[31m"; // cor vermelha
+    public static final String ANSI_RESET = "\u001B[0m"; // reset
+    public static final String ANSI_GREEN = "\u001B[32m"; // cor verde
+
+    public static void main(String[] args) {
+        ClearConsole Clearconsole = new ClearConsole();
         Scanner Leitura = new Scanner(System.in); // Instanciando a entrada de dados por meio da leitura
 
         boolean Execute = true;
 
         while(Execute == true) {
 
-            System.out.println("Combinador de Pdf's!\n1 - Executar Programa\n2 - Sair");
+            System.out.println("Combinador de Pdf's!");
+            System.out.println("\n1 - Executar Programa\n2 - Sair");
 
             String OpcaoUsuario = Leitura.nextLine();
 
              switch(OpcaoUsuario){
 
                 case "1":
+                    Clearconsole.Limpador();
                     System.out.println("Digite o caminho do diretório que contém os pdf's:");
                     String caminhoDaPasta = Leitura.nextLine();
-
+                    Clearconsole.Limpador();
                     System.out.println("Digite o nome do arquivo final");
                     String NomeArquivoFinal = "\\"+ Leitura.nextLine() + ".pdf";
-
+                    Clearconsole.Limpador();
                     System.out.println("Digite o caminho final.");
                     String destino = Leitura.nextLine() + NomeArquivoFinal;
 
@@ -32,25 +41,31 @@ public class PdfCombiner{
 
                     File pasta = new File(caminhoDaPasta);
                     File[] arquivos = pasta.listFiles((dir, name) -> name.toLowerCase().endsWith(".pdf"));
+
                     if (arquivos == null || arquivos.length == 0) {
-                        System.out.println("Nenhum PDF encontrado na pasta.");
-                        main(args);
+                        Clearconsole.Limpador();
+                        System.out.println("Nenhum PDF encontrado na pasta. Voltando ao início. . .\n");
+
                     }
 
                     PDFMergerUtility merger = new PDFMergerUtility();
                     merger.setDestinationFileName(destino);
 
                     try {
-
+                        Clearconsole.Limpador();
                         for (File pdf : arquivos) {
                             System.out.println("Adicionando: " + pdf.getName());
                             merger.addSource(pdf);
                         }
 
                         merger.mergeDocuments(null);
-                        System.out.println("PDFs unidos com sucesso! Arquivo final: " + destino);
+                        System.out.println(ANSI_GREEN + "PDFs unidos com sucesso! Arquivo final: " + ANSI_RESET + destino);
+                        System.out.println("Pressione ENTER para continuar. . .");
+                        Leitura.next();
+                        Clearconsole.Limpador();
+
                     } catch (IOException e) {
-                        System.err.println("Erro ao unir PDFs: " + e.getMessage());
+                        System.err.println(ANSI_RED + "Erro ao unir PDFs: " + e.getMessage());
                     }
 
 
@@ -58,6 +73,9 @@ public class PdfCombiner{
                     Execute = false;
                     break;
 
+                 default:
+                     System.out.println( ANSI_RED + "Comando não reconhecido!\n");
+                     main(args);
             }
 
         }
